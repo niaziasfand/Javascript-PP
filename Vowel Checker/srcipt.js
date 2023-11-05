d739aaf15266b489dac1f1413b19e48e585b42ed
Asfand Ali Khan <116268201+niaziasfand@users.noreply.github.com>
2023-11-05 11:19:32 +0500

    Add files via upload

---
+++ Vowel Checker/srcipt.js
@@ -0,0 +1,20 @@
+function checkvowels() {
+    var text = document.getElementById('textarea').value;
+    var vowelCount = 0;
+    text = text.toLowerCase();
+
+    for (var i = 0; i < text.length; i++) {
+        var char = text.charAt(i);
+        if (isVowel(char)) {
+            vowelCount++;
+        }
+    }
+
+    var result = document.getElementById('result');
+    result.textContent = "Total vowels: " + vowelCount;
+}
+
+function isVowel(char) {
+    var vowels = ["a", "e", "i", "o", "u"];
+    return vowels.includes(char);
+}
